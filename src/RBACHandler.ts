060edf284e4a09958e4a5dff0a6b7e85e80ef12b
Robert Smayda <smayda44@gmail.com>
2020-07-24 15:22:50 -0400

    First commit (#1)

---
+++ src/RBACHandler.ts
@@ -0,0 +1,66 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+import { decode } from 'jsonwebtoken';
+import {
+    Authorization,
+    AuthorizationRequest,
+    AuthorizationBundleRequest,
+    TypeOperation,
+    SystemOperation,
+    BatchReadWriteRequest,
+} from '@awslabs/aws-fhir-interface';
+import { Rule, RBACConfig } from './RBACConfig';
+
+// eslint-disable-next-line import/prefer-default-export
+export class RBACHandler implements Authorization {
+    private readonly version: number = 1.0;
+
+    private readonly rules: RBACConfig;
+
+    constructor(rules: RBACConfig) {
+        this.rules = rules;
+        if (this.rules.version !== this.version) {
+            throw Error('Configuration version does not match handler version');
+        }
+    }
+
+    isAuthorized(request: AuthorizationRequest): boolean {
+        const decoded = decode(request.accessToken, { json: true }) || {};
+        const groups: string[] = decoded['cognito:groups'] || [];
+
+        return this.isAllowed(groups, request.operation, request.resourceType);
+    }
+
+    async isBundleRequestAuthorized(request: AuthorizationBundleRequest): Promise<boolean> {
+        const decoded = decode(request.accessToken, { json: true }) || {};
+
+        const groups: string[] = decoded['cognito:groups'] || [];
+
+        const authZPromises: Promise<boolean>[] = request.requests.map(async (batch: BatchReadWriteRequest) => {
+            return this.isAllowed(groups, batch.operation, batch.resourceType);
+        });
+        const authZResponses: boolean[] = await Promise.all(authZPromises);
+        return authZResponses.every(Boolean);
+    }
+
+    private isAllowed(groups: string[], operation: TypeOperation | SystemOperation, resourceType?: string): boolean {
+        if (operation === 'read' && resourceType === 'metadata') {
+            return true; // capabilities statement
+        }
+        for (let index = 0; index < groups.length; index += 1) {
+            const group: string = groups[index];
+            if (this.rules.groupRules[group]) {
+                const rule: Rule = this.rules.groupRules[group];
+                if (
+                    rule.operations.includes(operation) &&
+                    ((resourceType && rule.resources.includes(resourceType)) || !resourceType)
+                ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
