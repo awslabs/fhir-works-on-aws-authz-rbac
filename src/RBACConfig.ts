862db41238f01f61e6d877b9b91217f2dbb86dc4
smayda <smayda@amazon.com>
2021-01-27 08:54:37 -0500

    chore: update dependencies

--- src/RBACConfig.ts
+++ src/RBACConfig.ts
@@ -5,15 +5,16 @@
 
 import { TypeOperation, SystemOperation } from 'fhir-works-on-aws-interface';
 
-export interface RBACConfig {
-    version: number;
-    groupRules: GroupRule;
+export interface Rule {
+    operations: (TypeOperation | SystemOperation)[];
+    resources: string[]; // This will be able to support any type of resource
 }
 
 export interface GroupRule {
     [groupName: string]: Rule;
 }
-export interface Rule {
-    operations: (TypeOperation | SystemOperation)[];
-    resources: string[]; // This will be able to support any type of resource
+
+export interface RBACConfig {
+    version: number;
+    groupRules: GroupRule;
 }
